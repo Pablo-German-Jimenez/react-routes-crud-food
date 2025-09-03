07135e10172a7f7e0dc89cb5e2464668ac12fe57
linkin <pablogermanjimenez7@gmail.com>
2025-09-03 15:49:55 -0300

    first commit with footer pioola

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import FooterBboyLinkinStyle from './components/FooterBboyLinkinStyle'
+
+function App() {
+ 
+
+  return (
+    <>
+    <main><p>breaker life!♪</p></main>
+    
+    <FooterBboyLinkinStyle/>
+    </>
+  )
+}
+
+export default App
